d36c1e0273f2c5c297778a2d720c1fea95a89155
Shrikar <69652900+ShrikarSwami@users.noreply.github.com>
2025-10-02 11:06:51 -0400

    Implement rms and hm functions in lab3.py
    Added implementations for the rms and hm functions, including helper functions for squaring and inversion. Updated name and pledge in lab3.py and lab3_report.txt, and documented collaborators and sources.

--- Labs/lab3/lab3.py
+++ Labs/lab3/lab3.py
@@ -2,11 +2,12 @@
 # Fill in your name and the Stevens Honor Code pledge on the following lines.
 # Failure to fill this in will result in deducted marks.
 #
-# Name:
+# Name: Shrikar Swami
 #
-# Pledge:
+# Pledge:I pledge my honor that I have abided by the Stevens Honor System.
 #
 from functools import reduce
+import math
 # === CS 115 Lab 3 ===
 #
 # You are welcome and encouraged to define additional functions here.
@@ -14,20 +15,39 @@ from functools import reduce
 def add(x,y):
     sum = x + y
     return sum
+
+def square(x):
+    return x ** 2
+
+def real(x):
+    if x is None:
+        return False
+    else:
+        return True
+def inverse(x):
+    return (1/x)
 # ---
 
 def mean(data):
     items = len(data)
     sum=reduce(add,(data))
-    return sum/items
+    return (sum/items)
 
 
 
 def rms(data):
-    pass  # Delete this line, and write your own code here instead.
-
+    data=list(filter(real,(data)))
+    length=len(data)
+    data=map(square,(data))
+    total=reduce(add,(data))
+    mean=total/length
+    return math.sqrt(mean)
 
 def hm(data):
-    pass  # Delete this line, and write your own code here instead.
+    data=list(filter(real,(data)))
+    length=len(data)
+    data=map(inverse,(data))
+    sum=reduce(add,(data))
+    return length/sum
 
 
