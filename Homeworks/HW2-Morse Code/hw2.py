478342f0c384a1b8acc2fff328b39c307f870ed9
Shrikar <69652900+ShrikarSwami@users.noreply.github.com>
2025-10-08 10:38:31 -0400

    Add letter_to_code function and update report details
    Introduced the letter_to_code function in hw2.py to map letters to Morse code. Updated hw2_report.txt with name, pledge, and additional reflections on the assignment.

--- Homeworks/HW2-Morse Code/hw2.py
+++ Homeworks/HW2-Morse Code/hw2.py
@@ -20,6 +20,14 @@ dictionary = ("AM", "AS", "BE", "BED", "CAN", "EGG", "HE", "HER", "HIM",
     "HIS", "ILL", "IS", "KID", "ME", "MY", "ON", "OR", "SEE", "SO", "TO",
     "TOE", "TOW", "WAS", "WOW",)
 
+def letter_to_code(letter):
+    matching_pairs = tuple(filter(lambda pair: pair[1] == letter, morse)) #keep only pairs from morse in which the second item (or the letter) matches the letter in the word
+    if matching_pairs: #Once it gets rid of every single letter that DOESN't match it then goes ahead and gets the morse code values
+        first_pair = matching_pairs[0] #This is taking the first pair
+        code = first_pair[0] #Then swapping the Letter to code
+        return code #This returns the morse
+    return "Error" #This is what happens when nothing is found or it breaks 
+
 # Uncomment one of the following lines to try your code
 # with a larger dictionary of words:
 #from dict import dictionary
@@ -27,8 +35,8 @@ dictionary = ("AM", "AS", "BE", "BED", "CAN", "EGG", "HE", "HER", "HIM",
 
 def encode(plaintext):
     plaintext = plaintext.upper() #Converts this to uppercase
-    letters = list(plaintext) #Takes the letters into individual list "HELLO" = ["H","E","L","L","O"]
-    
+    letters = list(plaintext) #Takes the letters into individual list "HELLO" = ['H','E','L','L','O']
+
     
 
 def decode(cyphertext):
