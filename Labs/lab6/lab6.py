0a7cebc9a991a474cb40ad72d7218376935f24c3
Shrikar <69652900+ShrikarSwami@users.noreply.github.com>
2025-10-23 10:12:44 -0400

    Implement base conversion functions in lab6.py
    Added implementations for to_int and to_base functions in lab6.py to handle conversions between integers and tuples in arbitrary bases. Updated lab6_report.txt and filled in name and pledge as required.

--- Labs/lab6/lab6.py
+++ Labs/lab6/lab6.py
@@ -2,14 +2,29 @@
 # Fill in your name and the Stevens Honor Code pledge on the following lines.
 # Failure to fill this in will result in deducted marks.
 #
-# Name:
+# Name: Shrikar Swami
 #
-# Pledge:
+# Pledge: I pledge my honor that I have abided by the Stevens Honor System.
 #
 # === CS 115 Lab 6 ===
-def to_int(tup, base):
-    pass # Delete this line, and write your own code here instead.
+def to_int(tup, base): #This is the line defining the function
+    if len(tup) == 1: #This is a base case that checks if there is only one value and if there is return the same thing
+        return tup[0]
+    else: #This is the actual part of the program that uses recursion to check add up each value
+        length = len(tup)
+        return tup[0]*base**(length-1)+ to_int(tup[1:], base) #This is the recursive statement that takes the value, multiply if by the base to the power of whatever the length 
+    
 
 def to_base(n, base):
-    pass # Delete this line, and write your own code here instead.
+    if n == 0:
+        return (0,)
+    elif n<base:
+        return (n,)
+    else:
+        tup = []
+        quotient = n//base
+        remainder = n%base
+        return to_base(quotient, base) + (remainder,)
+        
+            
  
