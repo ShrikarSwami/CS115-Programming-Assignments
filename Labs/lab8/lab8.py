0a6eafe68d5ff9b658d7e3b647d58a465fa5faf2
Shrikar <69652900+ShrikarSwami@users.noreply.github.com>
2025-11-06 13:26:12 -0500

    Lab 8 Completion
    Added the lab.py, the entire lab report, as well as the CA check in for those who may need it

--- Labs/lab8/lab8.py
+++ Labs/lab8/lab8.py
@@ -8,8 +8,22 @@
 #
 # === CS 115 Lab 8 ===
 def copy_3d(lll):
-    pass # Delete this line, and write your own code here instead.
+    return list(map(lambda mid: list(map(lambda inner: inner[:], mid) ), lll)) #This uses the higher order function lambda to recursively copy each item within the list
 
+
+def check(x):
+    """
+    This is a helper function that basically just gives me id as an int for me to compare
+    """
+    return id(x)
 
 def check_copy_3d(lll_1, lll_2):
-    pass # Delete this line, and write your own code here instead.
+    if isinstance(lll_1,list) and isinstance(lll_2, list): #Base case to make sure that the items are lists
+        if check(lll_1)==check(lll_2): #This is a base case that makes sure the two lists aren't pointing to the same memory
+            return False #If it does point to the same memory it throw back and error
+        if len(lll_1)!=len(lll_2): #If the lists are not same in size they cannot be a deep_copy
+            return False #That also returns an error
+        compare = map(lambda a, b: check_copy_3d(a, b), lll_1, lll_2) #This compares the id's of everything within the list going list by list then item by item within that list and then returinng all like [true, true, true] = True
+        return all(compare)
+    return lll_1==lll_2
+    
